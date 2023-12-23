e09d4073cf91956a963c0432a9912d3b1a4b2dc1
Marlvin Goremusandu <122947486+Marlvin12@users.noreply.github.com>
2023-12-22 20:45:20 -0600

    Add files via upload

---
+++ models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from numpy import exp
+
+#Create your models here.
+class Data(models.Model):
+    name=models.CharField(max_length=200)
+    price=models.IntegerField()
+    rent=models.IntegerField()
+    emi=models.IntegerField()
+    tax=models.IntegerField()
+    exp=models.IntegerField()
+    expenses_monthly=models.IntegerField(default=0)
+    income_monthly=models.IntegerField(default=0)
+    
